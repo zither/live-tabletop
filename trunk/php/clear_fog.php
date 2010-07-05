f5da74c2307d6e589e1e264ccfbed805aca8b556
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-05 21:39:33 +0000

    stubs for remaining php scripts

---
+++ trunk/php/clear_fog.php
@@ -0,0 +1,11 @@
+<?php
+
+// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+
+// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
+ 
+// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+
+// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+
+?>
