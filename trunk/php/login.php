920adbc32f55797b8f054e34875f2650460184c6
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-08 20:15:21 +0000

    implemented login script

--- trunk/php/login.php
+++ trunk/php/login.php
@@ -1,11 +1,45 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+include('db_config.php');
+include('users.php');
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
+// Failure for any reason results in an empty <users></users> document element.
+// We return same failure result regardless of the reason for failure so that
+// we don't help password crackers figure out if they got the wrong password
+// or the wrong username or the wrong argument names.
+
+$FAIL = "<users></users>\n";
+
+// STEP 1: Interpret the Request
+
+$username = mysqli_real_escape_string($_REQUEST['username']);
+$password = mysqli_real_escape_string($_REQUEST['password']);
+
+// STEP 2: Query the Database
+
+$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
+  or die($FAIL);
+
+$query = "CALL get_user_by_name($username)";
+$result = mysqli_query($query) or die($FAIL);
  
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+// STEP 3: Interpret the Result
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC)) or die($FAIL);
+$hash = LT_hash_password($password, $row['salt']);
+if (strcmp($hash, $row['hash']) != 0) die ($FAIL);
+
+// STEP 4: Save session variables that only the server can modify
+
+$_SESSION['user_id'] = $row['user_id'];
+$_SESSION['permissions'] = $row['permissions'];
+
+// STEP 5: Generate Output
+
+echo "<users>\n"
+   . "  <user id=\"{$row['user_id']}\" name=\"{$row['name']}\""
+   .     " color=\"{$row['color']}\" permissions=\"{$row['permissions']}\"/>\n"
+   . "</users>\n";
 
 ?>
+
