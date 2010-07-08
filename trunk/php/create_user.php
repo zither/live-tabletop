0db8c9f9fb7e91010e3c78701629b40bb63a619a
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-08 21:43:59 +0000

    check whether user is logged in before changing password. implemented create_user.php

--- trunk/php/create_user.php
+++ trunk/php/create_user.php
@@ -1,11 +1,27 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+include('db_config.php');
+include('users.php');
+session_start();
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
- 
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+if (!isset($_SESSION['user_id'])) die('You are not logged in.');
+if (strcmp($_SESSION['permissions'], 'administrator') != 0)
+  die ("You do not have permission to do this.");
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+// STEP 1: Interpret the Request
+
+$username = mysqli_real_escape_string($_REQUEST['username']);
+$password = mysqli_real_escape_string($_REQUEST['password']);
+$permissions = mysqli_real_escape_string($_REQUEST['permissions']);
+
+// STEP 2: Query the Database
+
+$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
+  or die('Could not connect: ' . mysqli_error());
+
+$salt = LT_random_salt();
+$hash = LT_hash_password($password, $salt);
+$query = "CALL create_user('$username', '$hash', '$salt', NULL, '$permissions')";
+mysqli_query($link, $query) or die('Query failed: ' . mysqli_error());
 
 ?>
--- trunk/php/update_user_password.php
+++ trunk/php/update_user_password.php
@@ -4,9 +4,9 @@ include('db_config.php');
 include('users.php');
 session_start();
 
-// STEP 1: Interpret the Request
-
 if (!isset($_SESSION['user_id'])) die('You are not logged in.');
+
+// STEP 1: Interpret the Request
 
 $user_id = mysqli_real_escape_string($_SESSION['user_id']);
 $password = mysqli_real_escape_string($_REQUEST['password']);
