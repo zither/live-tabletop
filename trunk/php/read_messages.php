138df27fc3a16ff72b6feef9e4ec52ac60c7c988
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-09 15:42:42 +0000

    finished message scripts

--- trunk/php/read_messages.php
+++ trunk/php/read_messages.php
@@ -1,37 +1,22 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+include('db_config.php');
+include('roll.php');
+session_start();
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
- 
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+if (!isset($_SESSION['user_id'])) die('You are not logged in.');
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+$table_id = mysqli_real_escape_string($_REQUEST['table_id']);
 
-header("Content-Type: text/xml");
-header("Cache-Control: no-cache, must-revalidate");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // a date in the past
-echo '<?xml version="1.0" encoding="utf-8"?>';
+echo "<messages>\n";
+if ($link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)) {
+  if ($result = mysqli_query($link, "CALL read_messages($table_id)")) {
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+      echo "  <message user_id=\"{$row['user_id']}\" time=\"{$row['time']}\">"
+        . htmlspecialchars($row['text']) . "</message>\n";
+    }
+  }
+}
+echo "</messages>\n";
+
 ?>
-<messages>
-  <message user_id="2" time="">
-    I cast Arcane Projectile, rolling 
-    &lt;span title="d20 + 8"&gt;17&lt;/span&gt;
-    to hit and
-    &lt;span title="2d4 + 1"&gt;5&lt;/span&gt;
-    for damage.
-  </message>
-  <message user_id="1" time="">
-    That's a miss.
-  </message>
-  <message user_id="2" time="">
-    You lie!
-  </message>
-  <message user_id="3" time="">
-    This is not a real chat log, just a stub.
-  </message>
-  <message user_id="1" time="">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </message>
-</messages>
-
