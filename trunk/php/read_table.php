f272541fb1759493b8f4621f3f5066d2c9a78ac7
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-09 18:18:21 +0000

    implemented table scripts

--- trunk/php/read_table.php
+++ trunk/php/read_table.php
@@ -1,11 +1,45 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+include('db_config.php');
+session_start();
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
- 
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+if (!isset($_SESSION['user_id'])) die('You are not logged in.');
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+// STEP 1: Interpret the Request
+
+$name = mysqli_real_escape_string($_REQUEST['table_id']);
+
+// STEP 2: Query the Database
+
+$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
+  or die ("Connect failed: " + mysqli_error());
+
+$query = "CALL read_table('$table_id')";
+$result = mysqli_query($link, $query) or die ("Query failed: " + mysqli_error());
+
+// STEP 3: Generate Output
+
+include('xml_headers.php');
+echo "<tables>\n";
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+  echo "  <table"
+    . " id=\"{$row['table_id']}\""
+    . " user=\"{$row['user_id']}\""
+    . " name=\"{$row['name']}\""
+    . " background=\"{$row['image_id']}\""
+    . " tile_rows=\"{$row['tile_rows']}\""
+    . " tile_columns=\"{$row['tile_columns']}\""
+    . " tile_width=\"{$row['tile_width']}\""
+    . " tile_height=\"{$row['tile_height']}\""
+    . " grid_width=\"{$row['grid_width']}\""
+    . " grid_height=\"{$row['grid_height']}\""
+    . " grid_thickness=\"{$row['grid_thickness']}\""
+    . " grid_color=\"{$row['grid_color']}\""
+    . " piece_stamp=\"{$row['piece_stamp']}\""
+    . " tile_stamp=\"{$row['tile_stamp']}\""
+    . " message_stamp=\"{$row['message_stamp']}\""
+    . "/>\n";
+}
+echo "</tables>\n";
 
 ?>
--- trunk/php/update_table.php
+++ trunk/php/update_table.php
@@ -1,11 +1,39 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+include('db_config.php');
+session_start();
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
- 
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+if (!isset($_SESSION['user_id'])) die('You are not logged in.');
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+// STEP 1: Interpret the Request
+
+$admin = strcmp($_SESSION['permissions'], 'administrator') == 0;
+$user_id = mysqli_real_escape_string($_SESSION['user_id']);
+
+$name = mysqli_real_escape_string($_REQUEST['name']);
+$background = mysqli_real_escape_string($_REQUEST['background']);
+
+$grid_width = mysqli_real_escape_string($_REQUEST['grid_width']);
+$grid_height = mysqli_real_escape_string($_REQUEST['grid_height']);
+$grid_thickness = mysqli_real_escape_string($_REQUEST['grid_thickness']);
+$grid_color = mysqli_real_escape_string($_REQUEST['grid_color']);
+
+// STEP 2: Query the Database
+
+$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
+  or die ("Connect failed: " + mysqli_error());
+
+// only admins may update other user's tables.
+$result = (mysqli_query($link, "CALL read_table($table_id)")
+  or die ("Query error: " + mysqli_error());
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+if (!$admin && $user_id != $row['user_id'])
+  die("You do not have permission to do this.");
+
+// update the table
+$query = "CALL update_table($table_id, '$name', $user_id, $background, "
+  . "$grid_width, $grid_height, $grid_thickness, '$grid_color')";
+$result = mysqli_query($link, $query)
+  or die ("Query failed: " + mysqli_error());
 
 ?>
