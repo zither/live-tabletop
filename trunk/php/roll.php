d172fee0990df52f8c85f207a2aa8e04d31cfe9f
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-09 06:15:16 +0000

    functions for evaluating dice notation and expanding dice notation withing message strings.

--- trunk/php/roll.php
+++ trunk/php/roll.php
@@ -1,11 +1,48 @@
 <?php
 
-// STEP 1: Interpret the request ($_GET, $_POST, $_REQUEST, $_COOKIE, etc.)
+// Users can put dice rolls into chat messages using square brackets like this:
+//
+//  "I cast arcane projectile: [d20 + 8] to hit, [3d-1] damage."
+//
+// * white space is ignored
+// * numbers before and after "d" are optional: d4 = 1d4, 3d = 3d6, d = 1d6
+// * you can use any number of die rolls, addition and subtraction operators
+// * you cannot use parentheses, multiplication or division
 
-// STEP 2: Query the database (get the $result of calling a MySQL prepared statement)
- 
-// STEP 3: interpret the result (convert $result into a PHP structure or determine success/failure)
+// returns the result of a dice roll in dice notation
+function LT_roll($roll) {
+  $total = 0
+  $addends = explode('+', $roll);
+  for ($i = 0; $i < count($addends); $i++) {
+    $subtractends = explode('-', $addends[$i]);
+    for ($j = 0; $j < count($subtractends); $j++) {
+      $difference = 0;
+      $operands = explode('d', $subtractends[$j]);
+      if (count($operands) == 1) $difference = intval($operands[0]);
+      if (count($operands) == 2) {
+        $dice = is_numeric($operands[0]) ? intval($operands[0]) : 1;
+        $sides = is_numeric($operands[1]) ? intval($operands[1]) : 6;
+        for ($k = 0; $k < $dice; $k++) $difference += rand(1, $sides);
+      }
+      $total += ($j == 0) ? $difference : -$difference;
+    }
+  }
+  return $total;
+}
 
-// STEP 4: generate output (echo XML based on PHP structure, or indicate success/failure)
+// Replaces text in square brackets with <span> elements 
+// whose title attributes are the text between the square bracket, 
+// but whose contents are the result of passing that text through LT_roll().
+// The title is displayed as a tooltip in most browsers.
+function LT_expand_rolls($string) {
+  $left = explode('[', $string);
+  $output = $left[0];
+  for ($i = 0; $i < count($left); $i++) {
+    $right = explode(']', $left[$i]);
+    $ouput .= (count($right) == 2) ? "<span title=\"{$right[0]}\">"
+      . LT_roll($right[0]) . "</span>{$right[1]}" : $left[$i];
+  }
+  return $output;
+}
 
 ?>
