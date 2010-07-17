ddc3c30f690a2180d48f595eff9336dd8a673bc6
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-17 15:03:12 +0000

    use mysqli in object-oriented style, create $LT_SQL object and automatically connect when you include db_config.php instead of $DBLocation, $DBUsername etc.

--- trunk/php/install.php
+++ trunk/php/install.php
@@ -5,53 +5,51 @@ if (file_exists('db_config.php')) die ("Live Tabletop is already installed.");
 include('users.php');
 
 
-// STEP 1: Interpret the Request
+// Interpret the Request
 
-$DBLocation = $_REQUEST['location'];
-$DBUsername = $_REQUEST['username'];
-$DBPassword = $_REQUEST['password'];
-$DBName     = $_REQUEST['database'];
+$location = $_REQUEST['location'];
+$username = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+$database = $_REQUEST['database'];
 
-$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
-  or die('Could not connect.');
+$LT_SQL = new mysqli($location , $username , $password, $database);
+if ($LT_SQL->connect_errno != 0) die('Could not connect.');
 
-$admin_username = mysqli_real_escape_string($link, $_REQUEST['admin_username']);
-$admin_password = mysqli_real_escape_string($link, $_REQUEST['admin_password']);
+$admin_username = $LT_SQL->real_escape_string($_REQUEST['admin_username']);
+$admin_password = $LT_SQL->real_escape_string($_REQUEST['admin_password']);
 
 
-// STEP 2: Query the Database
+// Create the Database Schema (tables and stored procedures)
 
-// create the database schema (tables and stored procedures)
-mysqli_autocommit($link, FALSE);
-if (mysqli_multi_query($link, file_get_contents('schema.sql'))) {
+$LT_SQL->autocommit(FALSE);
+if ($LT_SQL->multi_query(file_get_contents('schema.sql'))) {
   do {
-    $result = mysqli_store_result($link);
-    if (mysqli_errno($link) != 0) {
-      mysqli_rollback($link);
-      die("Query failed: " . mysqli_error($link));
+    $result = $LT_SQL->store_result();
+    if ($LT_SQL->errno != 0) {
+      $LT_SQL->rollback();
+      die("Query failed: " . $LT_SQL->error);
     }
-  } while (mysqli_next_result($link));
-  mysqli_commit($link);
+  } while ($LT_SQL->next_result());
+  $LT_SQL->commit();
 }
 else {
-  mysqli_rollback($link);
-  die("Query failed: " . mysqli_error($link));
+  $LT_SQL->rollback();
+  die("Query failed: " . $LT_SQL->error);
 }
 
-// create an administrator account
+// Create an Administrator Account
+
 $salt = LT_random_salt();
 $hash = LT_hash_password($admin_password, $salt);
 $query = "CALL create_user('$admin_username', '$hash', '$salt', NULL, 'administrator')";
-mysqli_query($link, $query) or die('Query failed: ' . mysqli_error($link));
+$LT_SQL->query($query) or die('Query failed: ' . $LT_SQL->error);
 
 
-// STEP 3: Create db_config.php
+// Create db_config.php
+
 file_put_contents('db_config.php',
   "<?php\n"
-  . "\$DBLocation = '$DBLocation';\n"
-  . "\$DBUsername = '$DBUsername';\n"
-  . "\$DBPassword = '$DBPassword';\n"
-  . "\$DBName = '$DBName';\n"
+  . "  \$LT_SQL = new mysqli('$location', '$username', '$password', '$database');\n"
   . "?>\n");
 
 
