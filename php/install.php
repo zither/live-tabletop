32c2023195104409758e5f0d147374078a20fe4f
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-17 06:09:29 +0000

    Tested this so it actually works now.

--- php/install.php
+++ php/install.php
@@ -7,41 +7,42 @@ include('users.php');
 
 // STEP 1: Interpret the Request
 
-$DBLocation = mysqli_real_escape_string($_REQUEST['location']);
-$DBUsername = mysqli_real_escape_string($_REQUEST['username']);
-$DBPassword = mysqli_real_escape_string($_REQUEST['password']);
-$DBName = mysqli_real_escape_string($_REQUEST['database']);
-$admin_username = mysqli_real_escape_string($_REQUEST['admin_username']);
-$admin_password = mysqli_real_escape_string($_REQUEST['admin_password']);
+$DBLocation = $_REQUEST['location'];
+$DBUsername = $_REQUEST['username'];
+$DBPassword = $_REQUEST['password'];
+$DBName     = $_REQUEST['database'];
+
+$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
+  or die('Could not connect.');
+
+$admin_username = mysqli_real_escape_string($link, $_REQUEST['admin_username']);
+$admin_password = mysqli_real_escape_string($link, $_REQUEST['admin_password']);
 
 
 // STEP 2: Query the Database
-
-$link = mysqli_connect($DBLocation , $DBUsername , $DBPassword, $DBName)
-  or die('Could not connect: ' . mysqli_error());
 
 // create the database schema (tables and stored procedures)
 mysqli_autocommit($link, FALSE);
 if (mysqli_multi_query($link, file_get_contents('schema.sql'))) {
   do {
     $result = mysqli_store_result($link);
-    if (mysqli_errno() != 0) {
+    if (mysqli_errno($link) != 0) {
       mysqli_rollback($link);
-      die("Query failed: " . mysqli_error());
+      die("Query failed: " . mysqli_error($link));
     }
-  } while (mysqli_next_result($link);
+  } while (mysqli_next_result($link));
   mysqli_commit($link);
 }
 else {
   mysqli_rollback($link);
-  die("Query failed: " . mysqli_error());
+  die("Query failed: " . mysqli_error($link));
 }
 
 // create an administrator account
 $salt = LT_random_salt();
 $hash = LT_hash_password($admin_password, $salt);
 $query = "CALL create_user('$admin_username', '$hash', '$salt', NULL, 'administrator')";
-mysqli_query($link, $query) or die('Query failed: ' . mysqli_error());
+mysqli_query($link, $query) or die('Query failed: ' . mysqli_error($link));
 
 
 // STEP 3: Create db_config.php
@@ -51,7 +52,7 @@ file_put_contents('db_config.php',
   . "\$DBUsername = $DBUsername;\n"
   . "\$DBPassword = $DBPassword;\n"
   . "\$DBName = $DBName;\n"
-  . "?>\n";
+  . "?>\n");
 
 
 ?>
