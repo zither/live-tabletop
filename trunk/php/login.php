ae276a84bf3133566ef315c68df87c04dae0b372
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-07-18 06:36:17 +0000

    don't need this.

--- trunk/php/login.php
+++ trunk/php/login.php
@@ -21,8 +21,8 @@ include('xml_headers.php');
 echo "<users>\n";
 if ($result = $LT_SQL->query("CALL read_user_by_name('$username')")) {
   if ($row = $result->fetch_assoc()) {
-    $hash = LT_hash_password($password, $row['salt']);
-    if (strcmp($hash, $row['hash']) == 0) {
+    $hash = LT_hash_password($password, $row['password_salt']);
+    if (strcmp($hash, $row['password_hash']) == 0) {
       // Save session variables that only the server can modify
       $_SESSION['user_id'] = $row['user_id'];
       $_SESSION['permissions'] = $row['permissions'];
