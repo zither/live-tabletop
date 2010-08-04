0bffd4fba7d8161d636f6d1532f4267d9a83cc42
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-08-04 01:34:37 +0000

    New object-oriented style code without global functions except for anonymous document event handlers. Also delayed rearranging DOM tree in Panel.bringToFront to avoid strange behavior.

--- js/panel.js
+++ js/panel.js
@@ -1,52 +1,63 @@
-var blankObject = function(){
-  this.outerPanel = 0;
-  this.panelBar = 0; this.innerPanel = 0};
-var selectedPanel = blankObject;
-var selectedBR = blankObject;
-var selectedTL = blankObject;
-var dragX = 0;
-var dragY = 0;
-var clickX = 0;
-var clickY = 0;
-var clickDragGap = 0;
-var clickDragGap = 0;
-var z = 100;
-var invertResize = 1;
+// GLOBAL VARIABLES
+
+LT.selectedPanel = null;
+LT.selectedBR = null;
+LT.selectedTL = null;
+LT.dragX = 0; // current horizontal mouse position
+LT.dragY = 0; // current vertical mouse position
+LT.clickX = 0; // relative horizontal position of mouse when the button was pressed
+LT.clickY = 0; // relative vertical position of mouse when the button was pressed
+LT.clickCornerX = 0;
+LT.clickCornerY = 0;
+LT.clickDragGap = 0;
+
+// PANEL CLASS CONSTRUCTOR
 
 LT.Panel = function ( panelName, buttonName, buttonLoc, xPos, yPos, panelWidth, panelHeight) {
-  var panel = this;
-  panel.outerPanel = new LT.element('div', { 'class' : 'outerPanel', 
+
+  var panel = this; // Remember the current 'this' during event handlers.
+
+  // Create Floating Panel ---------------------------------------------------
+
+  this.outerPanel = LT.element('div', {'class' : 'outerPanel', 
     'style' : 'left: ' + xPos + 'px; top: ' + yPos + 'px; visibility: hidden;'
   }, LT.tableTop);
-  this.outerPanel.onmousedown = function(){ panel.bringToFront(); };
-  panelTitle = new LT.element('div', { 'class' : 'title' }, panel.outerPanel);
-  var resizeTL = new LT.element('div', { 'class' : 'resizeTL' }, panelTitle);
-  resizeTL.onmousedown = function(){ selectedTL = panel;};
-  var titleStart = new LT.element('div', { 'class' : 'titleStart' }, panelTitle);
-  var titleCaption = new LT.element('div', { 'class' : 'titleCaption' },
-    panelTitle, panelName);
-  var titleEnd = new LT.element('div', { 'class' : 'titleEnd' }, panelTitle);
-  this.panelBar = new LT.element('div', { 'class' : 'panelBar', 
-    'style' : 'width: ' + (panelWidth - 36) + 'px;' }, panelTitle, ' ');
-  this.panelBar.onmousedown = function(){ selectedPanel = panel; };
-  var panelClose = new LT.element('div', { 'class' : 'close' }, panelTitle);
-  panelClose.onclick = function(){ panel.show(); };
-  this.innerPanel = new LT.element('div', { 'class' : 'innerPanel', 
+  this.outerPanel.onmousedown = function() {panel.bringToFront();};
+
+  // Top: includes top-left resize button, title, close button
+  var panelTitle = LT.element('div', {'class' : 'title'}, this.outerPanel);
+  var resizeTL = LT.element('div', {'class' : 'resizeTL'}, panelTitle);
+  resizeTL.onmousedown = function() {LT.selectedTL = panel;};
+  LT.element('div', {'class' : 'titleStart'}, panelTitle);
+  LT.element('div', {'class' : 'titleCaption'}, panelTitle, panelName);
+  LT.element('div', {'class' : 'titleEnd'}, panelTitle);
+  this.panelBar = LT.element('div', {'class' : 'panelBar', 
+    'style' : 'width: ' + (panelWidth - 36) + 'px;'}, panelTitle, ' ');
+  this.panelBar.onmousedown = function() {LT.selectedPanel = panel;};
+  var panelClose = LT.element('div', {'class' : 'close'}, panelTitle);
+  panelClose.onclick = function() {panel.show();};
+
+  // Middle: panelContent contains elements specific to each panel
+  this.innerPanel = LT.element('div', {'class' : 'innerPanel', 
     'style' : 'width: ' + panelWidth + 'px; height: ' + panelHeight + 'px;' 
-    }, panel.outerPanel);
-  this.panelContent = new LT.element('div', {}, panel.innerPanel, "");
-  var panelBottom = new LT.element('div', { 'class' : 'panelBottom' }, panel.outerPanel);
-  var panelBL = new LT.element('div', { 'class' : 'panelBL' }, panelBottom);
-  var panelBR = new LT.element('div', { 'class' : 'resizeBR' }, panelBottom);
-  panelBR.onmousedown = function(){ selectedBR = panel;};
-// Create Menu Button ------------------------------------------------------
-  this.buttonContainer = new LT.element('div', { 'class' : 'buttonUnchecked' }, buttonLoc);
-  this.buttonContainer.onclick = function(){ panel.show(); };
-  var buttonStart = new LT.element('div', { 'class' : 'buttonStart' }, panel.buttonContainer);
-  var buttonCaption = new LT.element('div', { 'class' : 'buttonCaption' },
-  this.buttonContainer, buttonName);
-  var buttonEnd = new LT.element('div', { 'class' : 'buttonEnd' }, panel.buttonContainer);
-  buttonCaption.onselectstart = function () { return false; };
+    }, this.outerPanel);
+  this.panelContent = LT.element('div', {}, this.innerPanel, "");
+
+  // Bottom: includes bottom-right resize button
+  var panelBottom = LT.element('div', {'class' : 'panelBottom'}, this.outerPanel);
+  var panelBL = LT.element('div', {'class' : 'panelBL'}, panelBottom);
+  var panelBR = LT.element('div', {'class' : 'resizeBR'}, panelBottom);
+  panelBR.onmousedown = function() {LT.selectedBR = panel;};
+
+  // Create Menu Button ------------------------------------------------------
+
+  this.buttonContainer = LT.element('div', {'class' : 'buttonUnchecked'}, buttonLoc);
+  this.buttonContainer.onclick = function() {panel.show();};
+  LT.element('div', {'class' : 'buttonStart'}, this.buttonContainer);
+  var buttonCaption = LT.element('div', {'class' : 'buttonCaption'},
+    this.buttonContainer, buttonName);
+  buttonCaption.onselectstart = function () {return false;};
+  LT.element('div', {'class' : 'buttonEnd'}, this.buttonContainer);
 }
 
 LT.Panel.prototype.show = function(){
@@ -54,127 +65,103 @@ LT.Panel.prototype.show = function(){
     this.bringToFront();
     this.buttonContainer.className = "buttonChecked";
     this.outerPanel.style.visibility = "visible";
-  }else{
+  } else {
     this.buttonContainer.className = "buttonUnchecked";
     this.outerPanel.style.visibility = "hidden";
   }
 }
 
-document.onmouseup = unSelect;
+LT.Panel.prototype.bringToFront = function(){
+  // Remember the current value of 'this' until the timeout executes.
+  var panel = this;
+  setTimeout(function () {
+    // We delay this until the event handler is finished to avoid strange
+    // behavior that happens when we modify the DOM during mousedown events.
+    LT.tableTop.removeChild(panel.outerPanel);
+    LT.tableTop.appendChild(panel.outerPanel);
+  }, 0);
+};
 
-function unSelect(){
-  selectedPanel = blankObject;
-  selectedBR = blankObject;
-  selectedTL = blankObject;
-  clickDragGap = 0;
-  return true
+// DRAGGING: Use document event handlers to move and resize windows.
+
+document.onmouseup = function () {
+  LT.selectedPanel = null;
+  LT.selectedBR = null;
+  LT.selectedTL = null;
+  LT.clickDragGap = 0;
 }
-//DRAGGING -----------------------------------------------------------------------
-// Detect if the browser is IE or not.
-// If it is not IE, we assume that the browser is NS.
-var IE = document.all?true:false;
-// If NS -- that is, !IE -- then set up for mouse capture
-if (!IE) document.captureEvents(Event.MOUSEMOVE)
-// Set-up to use getMouseXY function onMouseMove
-document.onmousemove = getMouseXY;
 
-function getMouseXY(e) {
-  if (IE) { // grab the x-y pos.s if browser is IE
-    dragX = event.clientX + document.body.scrollLeft;
-    dragY = event.clientY + document.body.scrollTop;
-  } else {  // grab the x-y pos.s if browser is NS
-    dragX = e.pageX;
-    dragY = e.pageY;
+document.onmousemove = function (e) {
+  if (!e) var e = window.event;
+
+  // grab the X and Y position of the mouse cursor
+  if (document.all) { // IE browser
+    LT.dragX = e.clientX + document.body.scrollLeft;
+    LT.dragY = e.clientY + document.body.scrollTop;
+  } else { // NS browser
+    LT.dragX = e.pageX;
+    LT.dragY = e.pageY;
   }
   
-  if( selectedPanel != blankObject ){
-    movePanel();
+  // Move panel.
+  if (LT.selectedPanel) {
+    var width = parseInt(LT.selectedPanel.innerPanel.style.width);
+    var height = parseInt(LT.selectedPanel.innerPanel.style.height);
+    if (LT.clickDragGap == 0) {
+      LT.clickX = LT.dragX - parseInt(LT.selectedPanel.outerPanel.style.left);
+      LT.clickY = LT.dragY - parseInt(LT.selectedPanel.outerPanel.style.top);
+      LT.clickDragGap = 1;
+   }
+    LT.dragX = Math.min(LT.dragX - LT.clickX, window.innerWidth - width - 25);
+    LT.dragY = Math.min(LT.dragY - LT.clickY, window.innerHeight - height - 61);
+    LT.dragX = Math.max(LT.dragX, 26);
+    LT.dragY = Math.max(LT.dragY, 6);
+    LT.selectedPanel.outerPanel.style.top  = LT.dragY + "px";
+    LT.selectedPanel.outerPanel.style.left = LT.dragX + "px";
   }
-  if( selectedBR != blankObject ){
-    resizePanel();
+
+  // Resize panel using the bottom-right handle.
+  if (LT.selectedBR) {
+    var panelX = parseInt(LT.selectedBR.outerPanel.style.left);
+    var panelY = parseInt(LT.selectedBR.outerPanel.style.top);
+    if (LT.clickDragGap == 0) {
+      LT.clickX = LT.dragX - parseInt(LT.selectedBR.innerPanel.style.width);
+      LT.clickY = LT.dragY - parseInt(LT.selectedBR.innerPanel.style.height);
+      LT.clickDragGap = 1;
+   }
+/*
+    LT.dragX = Math.min(LT.dragX - LT.clickX, 140);
+    LT.dragY = Math.min(LT.dragY - LT.clickY, 100);
+    LT.selectedBR.innerPanel.style.width = Math.max(LT.dragX,
+      window.innerWidth - panelX - 25) + "px";
+    LT.selectedBR.innerPanel.style.height = Math.max(LT.dragY,
+      window.innerHeight - panelY - 61) + "px";
+*/
+    LT.dragX = Math.max(LT.dragX - LT.clickX, 140);
+    LT.dragY = Math.max(LT.dragY - LT.clickY, 100);
+    LT.selectedBR.innerPanel.style.width = Math.min(LT.dragX,
+      window.innerWidth - panelX - 25) + "px";
+    LT.selectedBR.innerPanel.style.height = Math.min(LT.dragY,
+      window.innerHeight - panelY - 61) + "px";
+    LT.selectedBR.panelBar.style.width = (LT.dragX - 36) + "px";
   }
-  if( selectedTL != blankObject ){
-    resizeMove();
+
+  // Resize panel using the top-left handle.
+  if (LT.selectedTL) {
+    if (LT.clickDragGap == 0) {
+      LT.clickX = LT.dragX - parseInt(LT.selectedTL.outerPanel.style.left);
+      LT.clickY = LT.dragY - parseInt(LT.selectedTL.outerPanel.style.top);
+      LT.clickCornerX = LT.dragX + parseInt(LT.selectedTL.innerPanel.style.width);
+      LT.clickCornerY = LT.dragY + parseInt(LT.selectedTL.innerPanel.style.height);
+      LT.clickDragGap = 1;
+    }
+    LT.dragX = Math.min(Math.max(LT.dragX, LT.clickX + 6), LT.clickCornerX - 140);
+    LT.dragY = Math.min(Math.max(LT.dragY, LT.clickY + 26), LT.clickCornerY - 100);
+    LT.selectedTL.outerPanel.style.left = (LT.dragX - LT.clickX) + "px";
+    LT.selectedTL.outerPanel.style.top  = (LT.dragY - LT.clickY) + "px";
+    LT.selectedTL.innerPanel.style.width  = (LT.clickCornerX - LT.dragX) + "px";
+    LT.selectedTL.innerPanel.style.height = (LT.clickCornerY - LT.dragY) + "px";
+    LT.selectedTL.panelBar.style.width = (LT.clickCornerX - LT.dragX - 36) + "px";
   }
-}
-function movePanel(){
-  var panel = selectedPanel.outerPanel;
-  var panelW = parseInt(selectedPanel.innerPanel.style.width);
-  var panelH = parseInt(selectedPanel.innerPanel.style.height);
-  if( clickDragGap == 0 ) {
-    clickY = dragY - parseInt(panel.style.top);
-    clickX = dragX - parseInt(panel.style.left);
-    clickDragGap = 1;
-  }
-  dragX = dragX - clickX;
-  dragY = dragY - clickY;
-  if (dragX > window.innerWidth - panelW - 25 ){
-  dragX = window.innerWidth - panelW - 25 ;}
-  if (dragY > window.innerHeight - panelH - 61){
-  dragY = window.innerHeight - panelH - 61;}
-  if (dragY < 26){dragY = 26;}
-  if (dragX < 6){dragX = 6;}
-  panel.style.top  = dragY + "px";
-  panel.style.left = dragX + "px";
-  // lock to grid
-  //locX = (dragX / ' . $configTileWidth . ') + 1;
-  //locY = (dragY / ' . $configTileHeight . ');
-  return true
-}
-function resizePanel(){
-  var panel = selectedBR.innerPanel;
-  var bar = selectedBR.panelBar;
-  var panelH = parseInt(panel.style.height);
-  var panelWidth = parseInt(panel.style.width);
-  var panelX = parseInt(selectedBR.outerPanel.style.left);
-  var panelY = parseInt(selectedBR.outerPanel.style.top);
-  if( clickDragGap == 0 ) {
-    clickY = dragY - parseInt(panel.style.height);
-    clickX = dragX - parseInt(panel.style.width);
-    clickDragGap = 1;
-  }
-  dragX = dragX - clickX;
-  dragY = dragY - clickY;
-  if (dragY < 100){dragY = 100;}
-  if (dragX < 140){dragX = 140;}
-  if (panelX + dragX + 25 > window.innerWidth ){
-    panel.style.width = (window.innerWidth - panelX - 25) + "px";
-  } else {
-    panel.style.width = dragX + "px";
-  }
-  if (dragY + panelY + 61 > window.innerHeight){
-    panel.style.height = (window.innerHeight - panelY - 61) + "px";
-  } else {
-    panel.style.height = dragY + "px";
-  }
-  bar.style.width = (dragX - 36) + "px";
-  return true
-}
-function resizeMove(){
-  resizeOP = selectedTL.outerPanel;
-  resizeIP = selectedTL.innerPanel;
-  bar = selectedTL.panelBar;
-  if( clickDragGap == 0 ) {
-    clickY = dragY - parseInt(resizeOP.style.top);
-    clickX = dragX - parseInt(resizeOP.style.left);
-    clickCornerY = dragY + parseInt(resizeIP.style.height);
-    clickCornerX = dragX + parseInt(resizeIP.style.width);
-    clickDragGap = 1;
-  }
-  if (dragX < clickX + 6 ){dragX = clickX + 6;}
-  if (dragY < clickY + 26){dragY = clickY + 26;}
-  if (dragX > clickCornerX - 140){dragX = clickCornerX - 140;}
-  if (dragY > clickCornerY - 100){dragY = clickCornerY - 100;}
-  resizeOP.style.left = (dragX - clickX) + "px";
-  resizeOP.style.top  = (dragY - clickY) + "px";
-  resizeIP.style.width = (clickCornerX - dragX) + "px";
-  resizeIP.style.height  = (clickCornerY - dragY) + "px";
-  bar.style.width = (clickCornerX - dragX - 36) + "px";
-  return true
-}
-LT.Panel.prototype.bringToFront = function(){
-  LT.tableTop.removeChild(this.outerPanel);
-  LT.tableTop.appendChild(this.outerPanel);
-  this.innerPanel.removeChild(this.panelContent);
-  this.innerPanel.appendChild(this.panelContent);
-}
+};
+
