48a4d3ea2aefd18326b534a7a070620a83c0d76d
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-08-06 02:32:28 +0000

    get the image's width and height

--- js/image.js
+++ js/image.js
@@ -1,11 +1,22 @@
 LT.Image = function (element) {
+  var self = this;
   // Use array notation instead of dot notation for properties
   // because one of the properties (public) is a reserved keyword.
-  this['id'] = parseInt(element.getAttribute("id"));
-  this['file'] = decodeURIComponent(element.getAttribute("file"));
-  this['type'] = decodeURIComponent(element.getAttribute("type"));
-  this['user_id'] = parseInt(element.getAttribute("user_id"));
-  this['public'] = element.getAttribute("public") != "0";
+  self['id'] = parseInt(element.getAttribute("id"));
+  self['file'] = decodeURIComponent(element.getAttribute("file"));
+  self['type'] = decodeURIComponent(element.getAttribute("type"));
+  self['user_id'] = parseInt(element.getAttribute("user_id"));
+  self['public'] = element.getAttribute("public") != "0";
+  // get the height and width of the image
+  self.sample = LT.element("img", {src: self.getURL()});
+  self.sample.onload = function () {
+    self.width = self.sample.width;
+    self.height = self.sample.height;
+  };
+};
+
+LT.Image.prototype.getURL = function () {
+  return 'images/upload/' + this.type + '/' + this.file;
 };
 
 LT.Image.prototype.update = function (newUserID, newPublic) {
@@ -15,7 +26,7 @@ LT.Image.prototype.update = function (newUserID, newPublic) {
     'public': typeof(newPublic) == "boolean" ? newPublic : this['public']
   };
   LT.ajaxRequest("POST", "php/update_image.php", args, function () {return;});
-}
+};
 
 LT.Image.prototype.remove = function () {
   var args = {image_id: this['id']};
