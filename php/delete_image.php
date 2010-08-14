7e24db4557e9da23eda00c6fcb0a66dfe0b9fde5
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2010-08-14 16:45:00 +0000

    use mysqli multi_query and gobble up extra result sets for multiple stored procedure calls in one php file.

--- php/delete_image.php
+++ php/delete_image.php
@@ -13,11 +13,16 @@ $image_id = $LT_SQL->real_escape_string($_REQUEST['image_id']);
 // Query the Database
 
 if (LT_can_modify_image($image_id)) {
-  $result = $LT_SQL->query("CALL read_image($image_id)")
+  $LT_SQL->multi_query("CALL read_image($image_id)")
     or die ("Query error: " . $LT_SQL->error);
+  $result = $LT_SQL->store_result();
   $row = $result->fetch_assoc();
   $type = $row['type'];
   $file = $row['file'];
+  if ($LT_SQL->more_results()) {
+    while($LT_SQL->next_result());
+  }
+
   $LT_SQL->query("CALL delete_image($image_id)")
     or die ("Query error: " . $LT_SQL->error);
   unlink(LT_image_path($type, $file))
