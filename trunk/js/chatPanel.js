f44ec6d7e96bf995a093f4985aad2c14899647c1
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2011-05-23 16:16:00 +0000

    compact timestamps

--- trunk/js/chatPanel.js
+++ trunk/js/chatPanel.js
@@ -20,11 +20,28 @@ LT.readChat = function(){
 LT.refreshMessageList = function () {
   LT.readChat();
   //var messagesArray = LT.sortObject(LT.messages, 'time');
-  for( var i=0; i < LT.messages.length; i++){
-      LT.element('span', {}, LT.chatOutput, LT.messages[i].userName +
-        ": " + LT.messages[i].time + ": ");
-      LT.chatOutput.appendChild(LT.messages[i].element);
-	  LT.element('br', {}, LT.chatOutput);
+  for (var i = 0; i < LT.messages.length; i++) {
+    // convert the time in seconds to a javascript date object
+    var then = new Date(LT.messages[i].time * 1000);
+    // get a javascript date object for the current date and time
+    var now = new Date();
+    // create an element to contain the timestamp
+    // the element's title (mouseover text) is the full date and time
+    var time = LT.element('span', {title: then.toString()}, LT.chatOutput);
+    if (then.toDateString() == now.toDateString()) {
+      // if the message is from today, then only show hours and minutes
+      time.textContent = "[" + then.getHours() + ":" + then.getMinutes() + "]";
+    } else {
+      // if the message is not from today, show year, month, day, hours and minutes
+      time.textContent = "[" + then.getFullYear() + "." + (then.getMonth() + 1) + "."
+        + then.getDate() + " " + then.getHours() + ":" + then.getMinutes() + "]";
+    }
+    // show the user who sent the message
+    LT.element('span', {}, LT.chatOutput, " " + LT.messages[i].userName + ": ");
+    // show the content of the message
+    LT.chatOutput.appendChild(LT.messages[i].element);
+    // start a new line after the message
+	LT.element('br', {}, LT.chatOutput);
   }
   
   LT.chatOutput.removeChild(LT.chatBottom);
