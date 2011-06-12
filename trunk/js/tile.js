af7185d14fcede205f8b51bfb1135e25ca084257
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2011-06-12 16:36:04 +0000

    refactored image and tile classes; tiles are now scaled to your grid size

--- trunk/js/tile.js
+++ trunk/js/tile.js
@@ -1,74 +1,94 @@
-// TILE CLASS CONSTRUCTOR
-LT.tileDragging = 0;
-
+// TILE CONSTRUCTOR
 LT.Tile = function (tableID, x, y, tileCode) {
   this.table_id = tableID;
   this.x = x;
   this.y = y;
   this.fog = parseInt(tileCode[0]);
   this.image_id = parseInt(tileCode.slice(1));
-  var tWidth = LT.currentTable.tile_width;
-  var tHeight = LT.currentTable.tile_height;
-  var tBackground = '';
-  if (this.image_id >= 0) {
-    tBackground = LT.tileImages[this.image_id].file;
-  }
-  this.clickDiv = LT.element('div', {'style': 'position: absolute; left: ' + (x * tWidth)
-    + 'px; top: ' + (y * tHeight) + 'px; width: ' + tWidth + 'px; height: ' + tHeight + 'px; '},
-    LT.clickTileLayer);
-  this.imageDiv = LT.element('div', {'style': 'position: absolute; left: ' + (x * tWidth)
-    + 'px; top: ' + (y * tHeight) + 'px; width: ' + tWidth + 'px; height: ' + tHeight + 'px; ' +    
-	' background: url(images/upload/tile/' + tBackground + ');'},
-    LT.tileLayer);
-  var self = this;
-  this.clickDiv.onclick = function (){
-
-  }
-  this.clickDiv.onmousedown = function() {
-    LT.tileDragging = 1;
-    self.imageDiv.style.backgroundImage = 'url(\'images/upload/tile/' + LT.selectedImage + '\')';
-    self.setImageID(LT.selectedImageID);
-  };
-  this.clickDiv.onmouseover = function() {
-    if (LT.tileDragging == 1){
-	  self.imageDiv.style.backgroundImage = 'url(\'images/upload/tile/' + LT.selectedImage + '\')';
-      self.setImageID(LT.selectedImageID);
-	}
-  };
-  //this.clickDiv = ;
+  this.createImage();
+  this.createClickableElement();
 };
 
-// TILE PROPERTIES
+// GLOBAL VARIABLES
+LT.Tile.PROPERTIES = ["fog", "image_id", "x", "y", "table_id"];
+LT.Tile.dragging = 0;
 
-LT.Tile.properties = ["fog", "image_id", "x", "y", "table_id"];
+// METHODS OF TILE OBJECTS
+LT.Tile.prototype = {
 
-// CLIENT-SERVER COMMUNICATION
-
-LT.Tile.prototype.update = function (mods) {
-  var args = {}
-  for (var i = 0; i < LT.Tile.properties.length; i++) {
-    var property = LT.Tile.properties[i];
-    if (typeof(mods[property]) != "undefined") {
-      this[property] = mods[property];
+  // CLIENT-SERVER COMMUNICATION
+  update: function (mods) {
+    var args = {}
+    for (var i = 0; i < LT.Tile.PROPERTIES.length; i++) {
+      var property = LT.Tile.PROPERTIES[i];
+      if (typeof(mods[property]) != "undefined") {
+        this[property] = mods[property];
+      }
+      args[property] = this[property];
     }
-    args[property] = this[property];
-  }
-  LT.ajaxRequest("POST", "php/update_tile.php", args, function () {return;});
-}
+    LT.ajaxRequest("POST", "php/update_tile.php", args, function () {return;});
+  },
+  remove: function () {
+    var args = {table_id: this.id, x: this.x, y: this.y};
+    LT.ajaxRequest("POST", "php/delete_tile.php", args, function () {return;});
+  },
 
-LT.Tile.prototype.remove = function () {
-  var args = {table_id: this.id, x: this.x, y: this.y};
-  LT.ajaxRequest("POST", "php/delete_tile.php", args, function () {return;});
-}
+  // PROPERTY ACCESSORS (GETTERS) AND MUTATORS (SETTERS)
+  getImageID: function () {return this.image_id;},
+  setImageID: function (newImageID) {
+    this.update({image_id: newImageID});
+    this.createImage();
+  },
+  hasFog: function () {return this.fog == 1;},
+  makeFog: function () {this.update({fog: 1});},
+  clearFog: function () {this.update({fog: 0});},
 
-// PROPERTY ACCESSORS (GETTERS) AND MUTATORS (SETTERS)
+  // CREATE A PROPERLY SCALED AND POSITIONED IMAGE
+  createImage: function () {
+    // if the tile already has an image, remove it
+    if (this.image) {
+      this.image.parentNode.removeChild(this.image);
+      delete(this.image);
+    }
+    // if the tile is not empty, create an image
+    if (this.image_id != -1) {
+      var image = LT.tileImages[this.image_id];
+      var tile_width = LT.currentTable.tile_width;
+      var tile_height = LT.currentTable.tile_height;
+      var xScale = tile_width /  image.tile_width;
+      var yScale = tile_height / image.tile_height;
+      var image_width = Math.round(image.width * xScale);
+      var image_height = Math.round(image.height * yScale);
+      var margin_left = -Math.round(image.center_x * xScale);
+      var margin_top = -Math.round(image.center_y * yScale);
+      var left = Math.round((this.x + 0.5) * tile_width);
+      var top = Math.round((this.y + 0.5) * tile_height);
+      this.image = LT.element('img', {'src' : image.getURL(),
+        'style': 'position: absolute; left: ' + left + 'px; top: ' + top + 'px; '
+        + 'width: ' + image_width + 'px; height: ' + image_height + 'px; '
+        + 'margin-left: ' + margin_left + 'px; margin-top: ' + margin_top + 'px; '},
+        LT.tileLayer);
+    }
+  },
 
-LT.Tile.prototype.getImageID = function () {return this.image_id;};
-LT.Tile.prototype.setImageID = function (newImageID) {
-  this.update({image_id: newImageID});
+  // CREATE AN ELEMENT YOU CAN CLICK ON TO CHANGE THE TILE
+  createClickableElement: function () {
+    var tile_width = LT.currentTable.tile_width;
+    var tile_height = LT.currentTable.tile_height;
+    this.clickDiv = LT.element('div', {'style': 'position: absolute; '
+      + 'left: ' + (this.x * tile_width) + 'px; top: ' + (this.y * tile_height) + 'px; '
+      + 'width: ' + tile_width + 'px; height: ' + tile_height + 'px; '},
+      LT.clickTileLayer);
+    var self = this;
+    this.clickDiv.onmousedown = function() {
+      LT.Tile.dragging = 1;
+      self.setImageID(LT.selectedImageID);
+    };
+    this.clickDiv.onmouseover = function() {
+      if (LT.Tile.dragging == 1) {
+        self.setImageID(LT.selectedImageID);
+	  }
+    };
+  },
 };
-
-LT.Tile.prototype.hasFog = function () {return this.fog == 1;};
-LT.Tile.prototype.makeFog = function () {this.update({fog: 1});};
-LT.Tile.prototype.clearFog = function () {this.update({fog: 0});};
 
