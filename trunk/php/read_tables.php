8ad7bcc4c217c5ad1c52af029ca8b3ed9ff9d891
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2011-06-28 03:01:30 +0000

    show all tables, regardless of who is logged in.

--- trunk/php/read_tables.php
+++ trunk/php/read_tables.php
@@ -6,13 +6,9 @@ if (!isset($_SESSION['user_id'])) die ('You are not logged in.');
 include('db_config.php');
 include('include/query.php');
 
-// Interpret the Request
-
-$user_id = $LT_SQL->real_escape_string($_SESSION['user_id']);
-
 // Query the Database
 
-$rows = LT_call('read_tables_by_user_id', $user_id);
+$rows = LT_call('read_tables');
 
 // Generate Output
 
