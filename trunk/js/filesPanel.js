5ce1f7dc701cbf6013b07529104002e2f64011dc
sethgalbraith <sethgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2011-08-01 04:18:00 +0000

    commented out debugging code correct location of PHP script

--- trunk/js/filesPanel.js
+++ trunk/js/filesPanel.js
@@ -1,15 +1,20 @@
 LT.createFilesPanel = function () {
   LT.filesPanel = new LT.Panel( 'Files', 'Files', 6, 118, 250, 150);
   
-  var my_uploader = new LT.Uploader("create_image.php", LT.filesPanel.content);
+  var my_uploader = new LT.Uploader("php/create_image.php", LT.filesPanel.content);
   my_uploader.setArgument("type", "tile");
   LT.element("input", {type:"submit", value:"upload"}, my_uploader.form);
-  
-my_uploader.form.onsubmit = function () {
-  alert('sending');
-}
+
+/*
+  my_uploader.form.onsubmit = function () {
+    alert('sending');
+  }
+*/
+
+/*
   my_uploader.onload = function (result) {
     alert(result);
   }
+*/
 };
 
