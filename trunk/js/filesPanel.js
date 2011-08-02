e9645a56ed3b8bdcf16ae488ae45781c48ee0693
charlesfgalbraith <charlesfgalbraith@7f290030-a168-f255-c0f6-6edfb17364db>
2011-08-02 14:45:11 +0000

    Added radio buttons for uploading files. Renamed pieces panel.

--- trunk/js/filesPanel.js
+++ trunk/js/filesPanel.js
@@ -2,7 +2,12 @@ LT.createFilesPanel = function () {
   LT.filesPanel = new LT.Panel( 'Files', 'Files', 6, 118, 250, 150);
   
   var my_uploader = new LT.Uploader("php/create_image.php", LT.filesPanel.content);
-  my_uploader.setArgument("type", "tile");
+  LT.element("input", {type:"radio", name:"type", value:"piece", checked:""}, 
+    LT.element('div', {}, my_uploader.form, "Piece"));
+  LT.element("input", {type:"radio", name:"type", value:"tile"}, 
+    LT.element('div', {}, my_uploader.form, "Tile"));
+  LT.element("input", {type:"radio", name:"type", value:"background"},
+    LT.element('div', {}, my_uploader.form, "Background"));
   LT.element("input", {type:"submit", value:"upload"}, my_uploader.form);
 
 /*
