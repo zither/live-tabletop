f8c7205cf61d0ffecbbe26ea8046b817dbea5635
sethgalbraith <sethgalbraith@gmail.com>
2014-04-13 18:00:15 +0000

    The Message class is no longer used.

--- js/Message.js
+++
@@ -1,8 +0,0 @@
-// TODO: this class is no longer used, so remove it
-LT.Message = function (data) {
-	this.user_id = data.user_id;
-	this.id      = data.id;
-	this.time    = data.time;
-	this.element = $("<span>").html(data.text)[0];
-};
-
