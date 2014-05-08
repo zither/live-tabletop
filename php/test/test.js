35ed59fdef2f3dcde6d0c71dd606ed8127e7328a
sethgalbraith <sethgalbraith@gmail.com>
2014-05-08 17:33:56 +0000

    begin implementing new design, JSON and JQuery instead of XML and LT.element/LT.text/LT.ajax

--- php/test/test.js
+++ php/test/test.js
@@ -2,116 +2,129 @@
 
 var TEST = {
 
-  // TEST.index keeps track of which test is being run.
-  // You should not modify TEST.index.
-  index: 0,
+	// TEST.index keeps track of which test is being run.
+	// You should not modify TEST.index.
+	index: 0,
 
-  // TEST.tests is an array of tests (empty by default.)
-  // You should append or replace this with your own tests.
-  tests: [],
+	// TEST.tests is an array of tests (empty by default.)
+	// You should append or replace this with your own tests.
+	tests: [],
 
-  // TEST.start() starts the tests. It should not be called
-  // until the document is loaded (i.e. "onload = TEST.start;".)
-  start: function () {
-    LT.element(document.body, ["STARTING TESTS ..."]);
-    TEST.request();
-  },
+	// TEST.start() starts the tests. It should not be called
+	// until the document is loaded (i.e. "onload = TEST.start;".)
+	start: function () {
+		$("<div>").text("STARTING TESTS ...").appendTo($("body"));
+		TEST.request();
+	},
 
-  // TEST.request() sends the ajax request for the current test.
-  // Do not use this method in your own functions and methods.
-  request: function () {
-    var test = TEST.tests[TEST.index];
-    if (test.uploader) {
-      test.uploader.submit(test.args);
-    }
-    else {
-      LT.ajaxRequest("POST", test.action, test.args, TEST.finish);
-    }
-  },
+	// TEST.request() sends the ajax request for the current test.
+	// Do not use this method in your own functions and methods.
+	request: function () {
+		var current_test = TEST.tests[TEST.index];
+		if (current_test.uploader)
+			current_test.uploader.submit(current_test.args);
+		else $.ajax({
+			type: "POST",
+			url: current_test.action,
+			data: current_test.args,
+			complete: TEST.finish
+		});
+	},
 
-  // TEST.blank(ajax) returns "PASS" if the returned XMLHttpRequest object
-  // (the ajax parameter) contains nothing but whitespace.
-  blank: function (ajax) {
-    var text = ajax.responseText.replace(/^\s+|\s+$/g, '');
-    if (text == "") {
-      return "PASS";
-    }
-    return "FAIL [" + text + "]";
-  },
+	// TEST.blank(ajax) returns "PASS" if the returned XMLHttpRequest object
+	// (the ajax parameter) contains nothing but whitespace.
+	blank: function (ajax) {
+		var text = ajax.responseText.replace(/^\s+|\s+$/g, '');
+		if (text == "") {
+			return "PASS";
+		}
+		return "FAIL [" + text + "]";
+	},
 
-  // TEST.exists(ajax) returns "PASS" if the returned XMLHttpRequest object
-  // (the ajax parameter) has the status 200 OK instead of 404 Not Found
-  // or some other status.
-  exists: function (ajax) {
-    if (ajax.status == 200) {
-      return "PASS";
-    }
-    return "FAIL [status = " + ajax.status + "]";
-  },
+	// TEST.exists(ajax) returns "PASS" if the returned XMLHttpRequest object
+	// (the ajax parameter) has the status 200 OK instead of 404 Not Found
+	// or some other status.
+	exists: function (ajax) {
+		if (ajax.status == 200) {
+			return "PASS";
+		}
+		return "FAIL [status = " + ajax.status + "]";
+	},
 
-  // TEST.unimplemented(ajax) returns "FAIL (test not implemented)" followed
-  // by the text of the returned XMLHttpRequest object (the ajax parameter.)
-  unimplemented: function (ajax) {
-    if (ajax && ajax.responseText) {
-      return "FAIL (test not implemented) [" + ajax.responseText + "]";
-    }
-    return "FAIL (test not implemented) [no XMLHttpRequest]";
-  },
+	// TEST.unimplemented(ajax) returns "FAIL (test not implemented)" followed
+	// by the text of the returned XMLHttpRequest object (the ajax parameter.)
+	unimplemented: function (ajax) {
+		if (ajax && ajax.responseText) {
+			return "FAIL (test not implemented) [" + ajax.responseText + "]";
+		}
+		return "FAIL (test not implemented) [no XMLHttpRequest]";
+	},
 
-  // TEST.uploaded(result) returns "PASS" if result is not a string.
-  // Uploader tests do not have an XMLHttpRequest object.
-  // Instead the server must return javascript which passes a result
-  // to the TEST.finish method.
-  uploaded: function (result) {
-    if (typeof (result) == 'string') {
-      return "FAIL [" + result + "]";
-    }
-    return "PASS";
-  },
+	// TEST.uploaded(result) returns "PASS" if result is not a string.
+	// Uploader tests do not have an XMLHttpRequest object.
+	// Instead the server must return javascript which passes a result
+	// to the TEST.finish method.
+	uploaded: function (result) {
+		if (typeof (result) == 'string') {
+			return "FAIL [" + result + "]";
+		}
+		return "PASS";
+	},
 
-  // TEST.count(ajax, tag, target) returns "PASS" if the returned
-  // XMLHttpRequest object (the ajax parameter) is an XML document containing
-  // the specified number (target) of the specified type (tag) of elements.
-  count: function (ajax, tag, target) {
-    if (!ajax.responseXML) {
-      return "FAIL [" + ajax.responseText + "]";
-    }
-    var count = ajax.responseXML.getElementsByTagName(tag).length;
-    if (count == target) {
-      return "PASS";
-    }
-    return "FAIL [returned " + count + " " + tag + "s]";
-  },
+	// TEST.count(ajax, target) returns "PASS" if the response
+	// is a JSON array with the specified length (target.)
+	count: function (ajax, target) {
+		try {var data = JSON.parse(ajax.responseText);}
+		catch (e) {return "FAIL [parse error: " + e + "]";}
+		if (typeof(data.length) == "undefined")
+			return "FAIL [data is a " + typeof(data.length) + "]";
+		if (data.length == target) return "PASS";
+		else return "FAIL [returned " + data.length + "objects]";
+	},
 
-  // TEST.equals(ajax, target) returns "PASS" if the returned XMLHttpRequest
-  // object (the ajax parameter) contains the specified text (target) after
-  // stripping leading and trailing whitespace.
-  equals: function (ajax, target) {
-    var text = ajax.responseText.replace(/^\s+|\s+$/g, '');
-    return (text == target) ? "PASS" : "FAIL [" + text + "]";
-  },
+	// TEST.one(ajax) and TEST.zero(ajax) are shorthand for
+	// TEST.count(ajax, 1) and TEST.count(ajax, 0).
+	// They can also be directly used as test.result callbacks
+	// because they only require an ajax argument.
+	one: function (ajax) {return TEST.count(ajax, 1);},
+	zero: function (ajax) {return TEST.count(ajax, 0);},
 
-  // TEST.finish(ajax) is called when each ajax request is completed.
-  // You should not use this method from your own functions and methods.
-  finish: function (ajax) {
-    var old_test = TEST.tests[TEST.index];
-    var result = old_test.result(ajax);
-    LT.element(document.body, [old_test.action + ": " + result],
-      {'class': result.slice(0,4)});
-    if (old_test.abort && result != "PASS") {
-      LT.element(document.body, ["... STOPPING TESTS!"]);
-      return;
-    }
-    TEST.index++;
-    if (TEST.index == TEST.tests.length) {
-      LT.element(document.body, ["... FINISHED!"]);
-      return;
-    }
-    var new_test = TEST.tests[TEST.index];
-    if (new_test.group) {
-      LT.element(document.body, [new_test.group]);
-    }
-    TEST.request();
-  }
+	// TEST.string(ajax, target) returns "PASS" if the response
+	// contains the specified text (target)
+	// after stripping leading and trailing whitespace.
+	string: function (ajax, target) {
+		var text = ajax.responseText.replace(/^\s+|\s+$/g, '');
+		return (text == target) ? "PASS" : "FAIL [" + text + "]";
+	},
+
+	// TEST.json(ajax, target) returns "PASS" if the response
+	// is JSON equivalent to the specified javascript object (target.)
+	json: function (ajax, target) {
+		try {var data = JSON.parse(ajax.responseText);}
+		catch (e) {return "FAIL [parse error: " + e + "]";}
+		if (JSON.stringify(data) == JSON.stringify(target)) return "PASS";
+		else return "FAIL [" + ajax.responseText + "]";
+	},
+
+	// TEST.finish(ajax) is called when each ajax request is completed.
+	// You should not use this method from your own functions and methods.
+	finish: function (ajax) {
+		var old_test = TEST.tests[TEST.index];
+		var result = old_test.result(ajax);
+		$("<div>").text(old_test.action + ": " + result).appendTo($("body"))
+			.addClass(result.slice(0, 4));
+		if (old_test.abort && result != "PASS") {
+			$("<div>").text("... STOPPING TESTS!").appendTo($("body"));
+			return;
+		}
+		TEST.index++;
+		if (TEST.index == TEST.tests.length) {
+			$("<div>").text("... FINISHED!").appendTo($("body"));
+			return;
+		}
+		var new_test = TEST.tests[TEST.index];
+		if (new_test.group) $("<div>").text(new_test.group).appendTo($("body"));
+		TEST.request();
+	}
 };
 
