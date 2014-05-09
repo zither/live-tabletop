e528a043e55145268de0b185227d73aab2def8cb
sethgalbraith@gmail.com <sethgalbraith@gmail.com@7f290030-a168-f255-c0f6-6edfb17364db>
2014-05-09 00:09:51 +0000

    php testing progress

--- branches/future/php/User.create.php
+++ branches/future/php/User.create.php
@@ -1,9 +1,6 @@
 <?php // User creates a new account for himself
 
 session_start();
-if (!isset($_SESSION['user_id'])) die ('You are not logged in.');
-if (strcmp($_SESSION['permissions'], 'administrator') != 0)
-  die ("You do not have permission to do this.");
 
 include('db_config.php');
 include('include/query.php');
@@ -11,11 +8,10 @@ include('include/password.php');
 
 // Interpret the Request
 
-$login       = $LT_SQL->real_escape_string($_REQUEST['login']);
-$password    = $LT_SQL->real_escape_string($_REQUEST['password']);
-$permissions = $LT_SQL->real_escape_string($_REQUEST['permissions']);
-$email       = $LT_SQL->real_escape_string($_REQUEST['email']);
-$subscribed  = $LT_SQL->real_escape_string($_REQUEST['subscribed']); // 0 or 1
+$login = $LT_SQL->real_escape_string($_REQUEST['login']);
+$password = $LT_SQL->real_escape_string($_REQUEST['password']);
+$email = $LT_SQL->real_escape_string($_REQUEST['email']);
+$subscribed = $LT_SQL->real_escape_string($_REQUEST['subscribed']); // 0 or 1
 
 $salt = LT_random_salt();
 $hash = LT_hash_password($password, $salt);
