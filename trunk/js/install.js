de40b414ddb5fee069652d77978086f34f5e5d15
sethgalbraith@gmail.com <sethgalbraith@gmail.com@7f290030-a168-f255-c0f6-6edfb17364db>
2014-06-16 12:41:08 +0000

    automatic install option

--- trunk/js/install.js
+++ trunk/js/install.js
@@ -1,4 +1,6 @@
 $(function () { // This anonymous function runs after the page loads.
+
+	// installer submit button
 	$("#installBox input[type=button]").click(function () {
 		var formData = LT.formValues("#installBox");
 		if (formData.admin_password != formData.retype_password) {
@@ -17,5 +19,29 @@ $(function () { // This anonymous function runs after the page loads.
 			});
 		});
 	});
+
+	// automatic install
+	if (window.location.search.length > 1) {
+		// get args from URL
+		var args = {}
+		var pairs = window.location.search.substr(1).split("&");
+		for (var i = 0; i < pairs.length; i++) {
+			var parts = pairs[i].split("=");
+			args[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+		}
+		// fill out form
+		$("#installBox input[name=location]").val(args.location);
+		$("#installBox input[name=database]").val(args.database);
+		$("#installBox input[name=username]").val(args.username);
+		$("#installBox input[name=password]").val(args.password);
+		$("#installBox input[name=admin_login]").val(args.admin_login);
+		$("#installBox input[name=admin_password]").val(args.admin_password);
+		$("#installBox input[name=retype_password]").val(args.admin_password);
+		// submit form
+		if (args.location && args.database && args.username && args.password
+			&& args.admin_login && args.admin_password)
+				$("#installBox input[type=button]").click();
+	}
+
 });
 
