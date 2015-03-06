b365e3fb000962faaae05d09cd84b895fcc42d8b
sethgalbraith <sethgalbraith@gmail.com>
2015-03-06 06:39:07 +0000

    fudge dice; sharp edged fog adjacent to walls;

--- php/include/roll.php
+++ php/include/roll.php
@@ -12,22 +12,38 @@
 // returns the result of a dice roll in dice notation
 function LT_roll($roll) {
   $total = 0;
+	$string = "";
   $addends = explode('+', $roll);
   for ($i = 0; $i < count($addends); $i++) {
     $subtractends = explode('-', $addends[$i]);
     for ($j = 0; $j < count($subtractends); $j++) {
       $difference = 0;
-      $operands = explode('d', $subtractends[$j]);
+      $operands = explode('d', $subtractends[$j], 2);
       if (count($operands) == 1) $difference = intval($operands[0]);
       if (count($operands) == 2) {
-        $dice = is_numeric($operands[0]) ? intval($operands[0]) : 1;
-        $sides = is_numeric($operands[1]) ? intval($operands[1]) : 6;
-        for ($k = 0; $k < $dice; $k++) $difference += rand(1, $sides);
+				$dice = is_numeric($operands[0]) ? intval($operands[0]) : 1;
+				$sides = trim($operands[1]);
+				if ($sides == "f") $sides = "+ -"; // Fudge dice
+				if ($sides == "h") $sides = "SSSWWB"; // Hero Quest combat dice
+				if ($sides == "%") $sides = "100";
+				$length = strlen($sides);
+				if ($length == 0 or is_numeric($sides)) {
+					// numbered dice, six sided by default
+					$sides = length == 0 ? 6 : intval($sides);
+					for ($k = 0; $k < $dice; $k++)
+						$difference += rand(1, $sides);
+				} else {
+					// dice with symbols, represented as characters in a string
+					for ($k = 0; $k < $dice; $k++)
+						$string .= $sides[rand(0, $length - 1)];
+				}
       }
       $total += ($j == 0) ? $difference : -$difference;
     }
   }
-  return $total;
+	if (strlen($string) == 0) return $total;
+	if ($total == 0) return $string;
+	return "$total|$string";
 }
 
 // replace text in square brackets with corresponding dice rolls
