2a9a9b77adbae6c0481372939e78616b266ca0a2
sethgalbraith <sethgalbraith@gmail.com>
2015-03-14 01:44:18 +0000

    Center new pieces

--- php/Piece.create.php
+++ php/Piece.create.php
@@ -14,13 +14,17 @@ if (!isset($_SESSION['user'])) {
 
 $map = intval($_REQUEST['map']);
 $image = $LT_SQL->real_escape_string($_REQUEST['image']);
+$x = floatval($_REQUEST['x']);
+$y = floatval($_REQUEST['y']);
 
 // Query the Database
 
 if (LT_can_edit_map($map)) {
 	if ($rows = LT_call('create_piece', $map, $image)) {
+		$piece = intval($rows[0]['id']);
+		LT_call('update_piece_position', $piece, $x, $y);
 		include('include/json_headers.php');
-		echo json_encode(array('id' => intval($rows[0]['id'])));
+		echo json_encode(array('id' => $piece));
 	}
 }
 
